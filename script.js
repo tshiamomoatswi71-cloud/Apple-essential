1fb6752d21a703ecf3a6f4c88f3571924cb25b9a
Tshiamo Moatswi <tshiamomoatswi71@gmail.com>
2026-05-24 15:28:07 +0200

    commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("JavaScript is connected!");
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', function() {
+ document.body.classList.toggle('dark-mode');
+
+ if (document.body.classList.contains('dark-mode')) {
+  toggleButton.textContent = 'Light Mode';
+ } else {
+  toggleButton.textContent = 'Dark Mode';
+ }
+});
