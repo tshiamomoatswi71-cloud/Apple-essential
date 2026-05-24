7724fc0ac3703f67b5b070501ab102d3388480dc
Tshiamo Moatswi <tshiamomoatswi71@gmail.com>
2026-05-24 15:45:09 +0200

    commit
    Added typing animation effect

--- script.js
+++ script.js
@@ -9,3 +9,46 @@ toggleButton.addEventListener('click', function() {
   toggleButton.textContent = 'Dark Mode';
  }
 });
+
+const texts = [
+ "Hi, I'm Tshiamo!",
+ "I'm a Web Developer",
+ "Welcome to Apple Essential!"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typingElement = document.getElementById('typing-headline');
+
+function typeEffect() {
+
+ const currentText = texts[textIndex];
+
+ if (isDeleting) {
+  typingElement.textContent = currentText.substring(0, charIndex - 1);
+  charIndex--;
+ } else {
+  typingElement.textContent = currentText.substring(0, charIndex + 1);
+  charIndex++;
+ }
+
+ if (!isDeleting && charIndex === currentText.length) {
+  isDeleting = true;
+  setTimeout(typeEffect, 2000);
+  return;
+ }
+
+ if (isDeleting && charIndex === 0) {
+  isDeleting = false;
+  textIndex = (textIndex + 1) % texts.length;
+ }
+
+ const speed = isDeleting ? 50 : 100;
+
+ setTimeout(typeEffect, speed);
+
+}
+
+typeEffect();
