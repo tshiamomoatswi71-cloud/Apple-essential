0ce31bd0e2a1facae4ac2fb7de8ce649af390094
Tshiamo Moatswi <tshiamomoatswi71@gmail.com>
2026-05-24 15:52:53 +0200

    commit
    Added typing animation and back to top button

--- script.js
+++ script.js
@@ -52,3 +52,17 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', function() {
+ if (window.scrollY > 300) {
+  backToTopButton.style.display = 'block';
+ } else {
+  backToTopButton.style.display = 'none';
+ }
+});
+
+backToTopButton.addEventListener('click', function() {
+ window.scrollTo({ top: 0, behavior: 'smooth' });
+});
